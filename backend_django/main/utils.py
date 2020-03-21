207d0e72e36bc518a3210750868bcebec6e8555d
Mexson Fernandes <robomex2020@gmail.com>
2020-03-21 19:51:52 +0530

    (added) cache + db

--- backend_django/main/utils.py
+++ backend_django/main/utils.py
@@ -1,6 +1,19 @@
 import requests 
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
+from cache_memoize import cache_memoize
+from .models import IndiaCasesTableModel, MythsWHOModel
+from django.db import IntegrityError
 
+
+def save_in_db(model, data):
+    try:
+        obj = model(**data)
+        obj.save()
+    except IntegrityError as e:
+        pass
+
+
+@cache_memoize(300)
 def get_table_india(link):
     URL = link
     r = requests.get(URL)
@@ -31,9 +44,12 @@ def get_table_india(link):
     head = [head]
     head.append(output_rows)
 
+    # save data in db
+    save_in_db(IndiaCasesTableModel,{'table': head})
     return head
 
 
+@cache_memoize(300)
 def get_who_myths(link):
     data = {'title':[], 'src': []}
     URL = link
@@ -45,4 +61,6 @@ def get_who_myths(link):
             data['title'].append(row.text)
     for table_ro in table.findAll('div', attrs = {'class': 'list-view--item highlight-widget--content matching-height--item'}):
         data['src'].append(table_ro.a['href'])
+    for t, s in zip(data['title'], data['src']):
+        save_in_db(MythsWHOModel, {'title': t, 'src': s})
     return data
