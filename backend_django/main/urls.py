75090adbbd1f1b97caa9844538d9ec51cd9265a0
Ajay Thakur <thakurajay2537@gmail.com>
2020-03-21 13:29:50 +0530

    (added) api for india table

--- backend_django/main/urls.py
+++ backend_django/main/urls.py
@@ -1,8 +1,9 @@
-from django.urls import include, path
+from django.urls import path
 from django.contrib import admin
 from main import views
 
 urlpatterns = [
     path('', views.home),
-    path('myths', views.fetch_myths_who)
+    path('myths', views.fetch_myths_who),
+    path('india_data', views.live_india_data)
 ]
--- backend_django/main/views.py
+++ backend_django/main/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-import requests 
-from bs4 import BeautifulSoup 
+from main.utils import get_who_myths, get_table_india
+
 
 def home(request):
     return HttpResponse("COVIA working fine!")
 
 
 def fetch_myths_who(request):
-    data = {'title':[], 'src': []}
-    URL = "https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/myth-busters"
-    r = requests.get(URL)
-    soup = BeautifulSoup(r.content, 'html5lib')
-    table = soup.find('div', attrs = {'id':'PageContent_C003_Col01', 'class': 'sf_colsIn col-md-10'})
-    for row in table.findAll('h2'):
-        data['title'].append(row.text)
-    for table_ro in table.findAll('div', attrs = {'class': 'list-view--item highlight-widget--content matching-height--item'}):
-        data['src'].append(table_ro.a['href'])
-    return JsonResponse({
-        "status": True,
-        "data": data
-    })  
+    try:
+        data = get_who_myths("https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/myth-busters")
+        print(data)
+        return JsonResponse({
+            "status": True,
+            "data": data
+        }) 
+    except Exception as e:
+        print(str(e))
+        return JsonResponse({
+            "status": False
+        })
+
+
+def live_india_data(request):
+    try:
+        data = {}
+        data['table'] = get_table_india("https://www.mohfw.gov.in/")
+        return JsonResponse({
+            "status": True,
+            "data": data
+        })
+    except Exception as e:
+        print(str(e))
+        return JsonResponse({
+            "status": False
+        })
