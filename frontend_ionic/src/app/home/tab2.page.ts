4e95baf8b962b9d8d852860185308b57e9eb687e
Rahul Sharma <rahulsharma94595@gmail.com>
2020-03-23 02:17:41 +0530

    (added) questions section

--- frontend_ionic/src/app/home/tab2.page.ts
+++ frontend_ionic/src/app/home/tab2.page.ts
@@ -6,10 +6,24 @@ import { Router } from '@angular/router';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              ) {}
 
   questionSection(){
     this.router.navigate(['/questions']);
-  }
+    
+    //  this.geolocation.getCurrentPosition(
+    //   {maximumAge: 1000, timeout: 5000,
+    //   enableHighAccuracy: true }
+    //   ).then((resp) => {
+    //         alert(JSON.stringify( resp.coords));
+
+    //         this.lat=resp.coords.latitude
+    //         this.lng=resp.coords.longitude
+    //         },er=>{
+    //           alert('Can not retrieve Location')
+    //         }).catch((error) => {
+    //         alert('Error getting location - '+JSON.stringify(error))
+    //         });
+        }
 }
--- frontend_ionic/src/app/questions/questions.page.ts
+++ frontend_ionic/src/app/questions/questions.page.ts
@@ -14,4 +14,75 @@ export class QuestionsPage implements OnInit {
   home() {
    this.router.navigate(['/tabs']);
  }
+  dict = [
+  {
+    'question':'Gender',
+    'type': 'text',
+    'options':['Male', 'Female', 'Other'],
+    'answer': ''
+  },
+  {
+    'question':'Age',
+    'options':[],
+    'placeholder': 'Enter age',
+    'answer': ''
+  },
+  {
+    'question':'Have you recently traveled to an area with known local spread of COVID-19?',
+     'type': 'text',
+    'options':['Yes', 'No'],
+    'answer': ''
+  },
+  {
+    'question':'Have you come into close contact (within 6 feet) with someone who has a laboratory-confirmed COVID – 19 diagnoses in the past 14 days?',
+     'type': 'text',
+    'options':['Yes', 'No'],
+    'answer': ''
+  },
+  {
+    'question':'Do you have a fever (greater than 100.4 F > 38.0 C)?',
+     'type': 'text',
+    'options':['Yes', 'No'],
+    'answer': ''
+  },
+  {
+    'question':'Cough?',
+     'type': 'text',
+    'options':['Once in a day', '2-5 time in a day', 'More than that', 'No'],
+    'answer': ''
+  },
+  {
+    'question':'Shortness of breath?',
+     'type': 'text',
+    'options':['Yes', 'No'],
+    'answer': ''
+  },
+  {
+    'question':'Difficulty breathing?',
+     'type': 'text',
+    'options':['Yes', 'No'],
+    'answer': ''
+  },
+  {
+    'question':'Existing/History Medical Condition.',
+     'type': 'text',
+    'options':['Heart disease', 'Diabetes', 'Lung disease', 'Asthma', 'HIV', 'No'],
+    'answer': ''
+  }
+  ]
+  submitQues() {
+  }
+
+  setAns(index, ans) {
+    this.dict[index]['answer'] = ans;
+
+     var qs = document.getElementsByClassName('quest' + index)
+     for (var i=0; i<qs.length; i++) {
+       console.log(qs)
+       qs[i].setAttribute('color', 'light');
+       qs[i].style.color = "#fff";
+     }
+     document.getElementById(ans+index).setAttribute('color', 'primary');
+     document.getElementById(ans+index).style.color = "#136f61";
+  }
  }
