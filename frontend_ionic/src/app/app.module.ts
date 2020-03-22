c973004f86e9fcccca19a67c12e4c9490fc3197d
anshcena <anshcena.1996@gmail.com>
2020-03-22 22:20:36 +0530

    (pwa : sw-manifest)

--- frontend_ionic/src/app/app.module.ts
+++ frontend_ionic/src/app/app.module.ts
@@ -11,11 +11,13 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
     StatusBar,
     SplashScreen,
